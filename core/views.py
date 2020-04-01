78522676d8f622c92d7f6933a5cdb4bd3bc48769
nikita <nikita03565>
2020-04-01 15:05:56 +0300

    dummy example finished

--- core/views.py
+++ core/views.py
@@ -1,34 +1,38 @@
 from django_elasticsearch_dsl_drf.constants import (
+    LOOKUP_FILTER_TERMS,
     LOOKUP_FILTER_RANGE,
+    LOOKUP_FILTER_PREFIX,
+    LOOKUP_FILTER_WILDCARD,
+    LOOKUP_QUERY_IN,
     LOOKUP_QUERY_GT,
     LOOKUP_QUERY_GTE,
-    LOOKUP_QUERY_IN,
     LOOKUP_QUERY_LT,
     LOOKUP_QUERY_LTE,
-    SUGGESTER_COMPLETION,
-)
+    LOOKUP_QUERY_EXCLUDE,
+    SUGGESTER_COMPLETION)
 from django_elasticsearch_dsl_drf.filter_backends import (
-    DefaultOrderingFilterBackend,
-    FacetedSearchFilterBackend,
     FilteringFilterBackend,
+    IdsFilterBackend,
+    OrderingFilterBackend,
+    DefaultOrderingFilterBackend,
     SearchFilterBackend,
+    FacetedSearchFilterBackend,
     SuggesterFilterBackend,
 )
-from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from django_elasticsearch_dsl_drf.viewsets import BaseDocumentViewSet
+from django_elasticsearch_dsl_drf.pagination import PageNumberPagination
 
 from core.documents import CarDocument
 from core.serializers import CarDocumentSerializer
 
 
-class CarViewSet(DocumentViewSet):
+class CarViewSet(BaseDocumentViewSet):
     document = CarDocument
     serializer_class = CarDocumentSerializer
-    ordering = ('id',)
     lookup_field = 'id'
 
     filter_backends = [
         DefaultOrderingFilterBackend,
-        FacetedSearchFilterBackend,
         FilteringFilterBackend,
         SearchFilterBackend,
         SuggesterFilterBackend,
