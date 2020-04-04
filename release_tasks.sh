1241a3f610400a5793568b29e849680abc0d3cb5
nikita03565 <nikita03565@gmail.com>
2020-04-04 20:15:46 +0300

    removed unnecessary things

--- release_tasks.sh
+++ release_tasks.sh
@@ -2,11 +2,3 @@
 python manage.py makemigrations
 python manage.py migrate
 echo "y" | python manage.py search_index --rebuild
-cd frontend
-npm install
-ls
-npm run build
-ls
-cd ..
-python manage.py collectstatic --no-input
-ls
