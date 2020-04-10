079d29382bcebb6a3591812d3c773a2748d9a556
nikita03565 <nikita03565@gmail.com>
2020-04-10 20:06:23 +0300

    fixed url in suggest

--- src/API_Requests/search.js
+++ src/API_Requests/search.js
@@ -13,7 +13,7 @@ export const searchDreams = async (term) => {
 
 export const suggestDreams = async (field, term) => {
     const res = await axios({
-        url: `/api/desire_docs/?${field}_suggest__completion=${term}`,
+        url: `/api/desire_docs/suggest/?${field}_suggest__completion=${term}`,
         headers: withAuthHeader(),
     });
     return res;
