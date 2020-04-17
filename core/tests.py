63f59f6543ad5674fa23c6b7343fb14c67a0f796
nikita03565 <nikita03565@gmail.com>
2020-04-17 18:52:29 +0300

    added tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+
+class TagsTests(APITestCase):
+    def test_(self):
+        self.assertEqual(1, 1)
